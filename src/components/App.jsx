0d4a9891141035300190a9897a15530bb7976a3e
MateuszCharysz <charyszek@interia.pl>
2023-05-15 12:20:40 +0200

    correct backup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import Profile from './profile/profile';
+import user from './profile/user.json';
+import Statistics from './statistics/statistics';
+import data from './statistics/data.json';
+
+console.log(data)
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="God"
+      data={data} />
+    </>
   );
 };
